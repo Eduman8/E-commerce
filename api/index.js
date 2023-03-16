29953cc2aa5afd033878df3617a6c2fbfa711dd2
zevek <adris1337@gmail.com>
2023-03-16 16:33:12 -0300

    import syntax fix

--- api/index.js
+++ api/index.js
@@ -1,7 +1,7 @@
 const server = require("./src/app.js");
 const { conn } = require("./src/db.js");
 const port = process.env.PORT || 3001;
-const loadDb = require("./src//helpers/loadDb.js");
+const loadDb = require("./src/helpers/loadDb.js");
 
 // Syncing all the models at once.
 conn.sync({ force: false }).then(() => {
