fca67f0b0491b302e0fca4f5ef1a1293cbb6b320
Julian Rodriguez <9191.julian@gmail.com>
2023-03-16 19:36:25 -0500

    Update

--- client/src/components/validating/validating.jsx
+++ client/src/components/validating/validating.jsx
@@ -19,17 +19,6 @@ function Validating() {
   }
   return (
 
-    <div className="valida">
-      {!isAuthenticated ?
-        <>
-          <h1 id="validateTxt">Waiting for authentication information.</h1>
-          <Link to="/">
-            <button id="validateBtn" type="button" className="btn btn-success">Back</button>
-          </Link>
-        </> : window.location.href = "http://localhost:3000/home"
-      }
-    </div>
-
         <div className="valida">
           {!isAuthenticated?
           <>
